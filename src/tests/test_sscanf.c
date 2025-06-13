0239dd95f1595ba8619a8feda521a2f6cc3d589d
Selifanchik <selifanchik@mail.ru>
2025-06-13 03:00:30 +0300

    test_sscanf

--- src/tests/test_sscanf.c
+++ src/tests/test_sscanf.c
@@ -477,7 +477,7 @@ START_TEST(test_sscanf_octal) {
 END_TEST
 
 // Тест %s
-START_TEST(test_sscanf_str) {
+START_TEST(test_sscanf_str_width) {
   const char *str = "   1Hello, world! ";
   char a[20];
   char b[20];
@@ -486,6 +486,68 @@ START_TEST(test_sscanf_str) {
   s21_count = s21_sscanf(str, "%19s", b);
   ck_assert_str_eq(a, b);
   ck_assert_int_eq(count, s21_count);
+}
+END_TEST
+
+START_TEST(test_sscanf_string_with_asterisk) {
+  const char *str = "skip keep";
+  char a[100] = {0}, b[100] = {0};
+  int c1 = sscanf(str, "%*s %s", a);
+  int c2 = s21_sscanf(str, "%*s %s", b);
+  ck_assert_int_eq(c1, c2);
+  ck_assert_str_eq(a, b);  // Ожидаем "keep"
+}
+END_TEST
+
+START_TEST(test_sscanf_leading_space) {
+  const char *str = "     world";
+  char a[10];
+  char b[10];
+  int c1 = sscanf(str, "%s", a);
+  int c2 = s21_sscanf(str, "%s", b);
+  ck_assert_int_eq(c1, c2);
+  ck_assert_str_eq(a, b);  // Ожидаем "word"
+}
+END_TEST
+
+START_TEST(test_sscanf_string_with_punctuation) {
+  const char *str = "text,more";
+  char a[10];
+  char b[10];
+  int c1 = sscanf(str, "%s", a);
+  int c2 = s21_sscanf(str, "%s", b);
+  ck_assert_int_eq(c1, c2);
+  ck_assert_str_eq(a, b);  // Ожидаем "text,more"
+}
+END_TEST
+
+START_TEST(test_sscanf_only_spaces) {
+  const char *str = "   ";
+  char a[10];
+  char b[10];
+  int c1 = sscanf(str, "%s", a);
+  int c2 = s21_sscanf(str, "%s", b);
+  ck_assert_int_eq(c1, c2);  // Ожидаем 0
+}
+END_TEST
+
+START_TEST(test_sscanf_string_width_1) {
+  const char *str = "abc";
+  char a[100] = {0}, b[100] = {0};
+  int c1 = sscanf(str, "%1s", a);
+  int c2 = s21_sscanf(str, "%1s", b);
+  ck_assert_int_eq(c1, c2);
+  ck_assert_str_eq(a, b);  // Ожидаем "a"
+}
+END_TEST
+
+START_TEST(test_sscanf_width_longer_than_string) {
+  const char *str = "short";
+  char a[100] = {0}, b[100] = {0};
+  int c1 = sscanf(str, "%10s", a);
+  int c2 = s21_sscanf(str, "%10s", b);
+  ck_assert_int_eq(c1, c2);
+  ck_assert_str_eq(a, b);  // Ожидаем "short"
 }
 END_TEST
 
@@ -765,6 +827,85 @@ START_TEST(test_sscanf_repeated_chars) {
 }
 END_TEST
 
+START_TEST(test_sscanf_char_asterisk) {
+  const char *str = "CD";
+  char a, b;
+  int count = sscanf(str, "%*c%c", &a);  // пропускаем первый символ
+  int s21_count = s21_sscanf(str, "%*c%c", &b);
+  ck_assert_int_eq(count, s21_count);
+  ck_assert_int_eq(a, b);
+}
+END_TEST
+
+START_TEST(test_sscanf_char_newline) {
+  const char *str = "\n";
+  char a, b;
+  int count = sscanf(str, "%c", &a);
+  int s21_count = s21_sscanf(str, "%c", &b);
+  ck_assert_int_eq(count, s21_count);
+  ck_assert_int_eq(a, b);
+}
+END_TEST
+START_TEST(test_sscanf_all_ascii_chars) {
+  for (int i = 0; i < 128; ++i) {
+    char input_str[2] = {0};  // строка из одного символа + '\0'
+    input_str[0] = (char)i;
+
+    char a = 0, b = 0;
+    int count = sscanf(input_str, "%c", &a);
+    int s21_count = s21_sscanf(input_str, "%c", &b);
+
+    ck_assert_msg(count == s21_count, "Count mismatch for ASCII %d (0x%02X)", i,
+                  i);
+    if (count == 1 && s21_count == 1) {
+      ck_assert_msg(a == b,
+                    "Char mismatch for ASCII %d (0x%02X): std='%c' s21='%c'", i,
+                    i, a, b);
+    }
+  }
+}
+END_TEST
+
+START_TEST(test_sscanf_char_zero_byte) {
+  const char str[] = {'\0', 'Z', '\0'};
+  char a, b;
+  int count = sscanf(str, "%c", &a);
+  int s21_count = s21_sscanf(str, "%c", &b);
+  ck_assert_int_eq(count, s21_count);
+  ck_assert_int_eq(a, b);
+}
+END_TEST
+
+START_TEST(test_sscanf_char_multiple) {
+  const char *str = "AB";
+  char a, b, c, d;
+  int count = sscanf(str, "%c%c", &a, &b);
+  int s21_count = s21_sscanf(str, "%c%c", &c, &d);
+  ck_assert_int_eq(count, s21_count);
+  ck_assert_int_eq(a, c);
+  ck_assert_int_eq(b, d);
+}
+END_TEST
+
+START_TEST(test_sscanf_char_end_of_string) {
+  const char *str = "";
+  char a = 'x', b = 'y';
+  int count = sscanf(str, "%c", &a);
+  int s21_count = s21_sscanf(str, "%c", &b);
+  ck_assert_int_eq(count, s21_count);  // оба должны вернуть 0
+}
+END_TEST
+
+START_TEST(test_sscanf_char_with_trailing_space) {
+  const char *str = "X ";
+  char a, b;
+  int count = sscanf(str, "%c", &a);
+  int s21_count = s21_sscanf(str, "%c", &b);
+  ck_assert_int_eq(count, s21_count);
+  ck_assert_int_eq(a, b);
+}
+END_TEST
+
 // Тест для short
 START_TEST(test_sscanf_short_variants) {
   const char *str = "32767 -32768 377 65535 1a3f 1A3F";
@@ -817,58 +958,18 @@ START_TEST(test_sscanf_long_variants) {
 }
 END_TEST
 
-// Чтение символов с повторением %3c
-START_TEST(test_sscanf_ret_EOF) {
-  const char *str = "  %1 ";
-  int a, b = 234, c, d = 234;
-  int count, count_21;
-  count = sscanf(str, "%n%%%*d%d", &a, &b);
-  count_21 = s21_sscanf(str, "%n%%%*d%d", &c, &d);
-  ck_assert_int_eq(count, count_21);
-  ck_assert_int_eq(a, c);
-  ck_assert_int_eq(b, d);
-}
-END_TEST
-
-START_TEST(test_sscanf_string_with_asterisk) {
-  const char *str = "skip keep";
-  char a[100] = {0}, b[100] = {0};
-  int c1 = sscanf(str, "%*s %s", a);
-  int c2 = s21_sscanf(str, "%*s %s", b);
-  ck_assert_int_eq(c1, c2);
-  ck_assert_str_eq(a, b);
-}
-END_TEST
-
-START_TEST(test_sscanf_leading_space) {
-  const char *str = "     world";
-  char a[10];
-  char b[10];
-  int c1 = sscanf(str, "%s", a);
-  int c2 = s21_sscanf(str, "%s", b);
-  ck_assert_int_eq(c1, c2);
-  ck_assert_str_eq(a, b);
-}
-END_TEST
-
-START_TEST(test_sscanf_string_with_punctuation) {
-  const char *str = "text,more";
-  char a[10];
-  char b[10];
-  int c1 = sscanf(str, "%s", a);
-  int c2 = s21_sscanf(str, "%s", b);
-  ck_assert_int_eq(c1, c2);
-  ck_assert_str_eq(a, b);
-}
-END_TEST
-
-START_TEST(test_sscanf_only_spaces) {
-  const char *str = "   ";
-  char a[10];
-  char b[10];
-  int c1 = sscanf(str, "%s", a);
-  int c2 = s21_sscanf(str, "%s", b);
-  ck_assert_int_eq(c1, c2);
+START_TEST(test_sscanf_percent2) {
+  const char *str = "%%% %%%X";
+  char a = 0;
+  char b = 0;
+  int count_std = sscanf(str, "%%%c", &a);
+  printf("%d", count_std);
+  printf("%c", a);
+  int count_s21 = s21_sscanf(str, "%%%c", &b);
+  printf("%d", count_s21);
+  ck_assert_int_eq(count_std, count_s21);
+  ck_assert_int_eq(a, b);
+  ck_assert_int_eq(a, 'X');
 }
 END_TEST
 
@@ -909,7 +1010,13 @@ Suite *sscanf_suite(void) {
   tcase_add_test(tc_core, test_sscanf_e_long_double);
   tcase_add_test(tc_core, test_sscanf_E_f_g_G);
   tcase_add_test(tc_core, test_sscanf_octal);
-  tcase_add_test(tc_core, test_sscanf_str);
+  tcase_add_test(tc_core, test_sscanf_str_width);
+  tcase_add_test(tc_core, test_sscanf_string_with_asterisk);
+  tcase_add_test(tc_core, test_sscanf_leading_space);
+  tcase_add_test(tc_core, test_sscanf_string_with_punctuation);
+  tcase_add_test(tc_core, test_sscanf_only_spaces);
+  tcase_add_test(tc_core, test_sscanf_string_width_1);
+  tcase_add_test(tc_core, test_sscanf_width_longer_than_string);
   tcase_add_test(tc_core, test_sscanf_p);
   tcase_add_test(tc_core, test_sscanf_n);
   tcase_add_test(tc_core, test_sscanf_percent);
@@ -930,14 +1037,16 @@ Suite *sscanf_suite(void) {
   tcase_add_test(tc_core, test_sscanf_leading_zeros);
   tcase_add_test(tc_core, test_sscanf_space_char);
   tcase_add_test(tc_core, test_sscanf_repeated_chars);
+  tcase_add_test(tc_core, test_sscanf_char_asterisk);
+  tcase_add_test(tc_core, test_sscanf_char_newline);
+  tcase_add_test(tc_core, test_sscanf_all_ascii_chars);
+  tcase_add_test(tc_core, test_sscanf_char_zero_byte);
+  tcase_add_test(tc_core, test_sscanf_char_multiple);
+  tcase_add_test(tc_core, test_sscanf_char_end_of_string);
+  tcase_add_test(tc_core, test_sscanf_char_with_trailing_space);
   tcase_add_test(tc_core, test_sscanf_short_variants);
   tcase_add_test(tc_core, test_sscanf_long_variants);
-  tcase_add_test(tc_core, test_sscanf_ret_EOF);
-  tcase_add_test(tc_core, test_sscanf_string_with_asterisk);
-  tcase_add_test(tc_core, test_sscanf_leading_space);
-  tcase_add_test(tc_core, test_sscanf_string_with_punctuation);
-  tcase_add_test(tc_core, test_sscanf_only_spaces);
-
+  tcase_add_test(tc_core, test_sscanf_percent2);
   suite_add_tcase(s, tc_core);
   return s;
 }
