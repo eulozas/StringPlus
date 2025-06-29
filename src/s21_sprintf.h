c5818c8b6d995687e1b8033fbcb43408a291fff8
Danelle Lavern <danellel@sh-h8.nn.21-school.ru>
2025-06-29 17:58:55 +0300

    change name

--- src/s21_sprintf.h
+++ src/s21_sprintf.h
@@ -22,29 +22,29 @@ typedef struct {
 } flags;
 
 int parsing(const char* format, int* i, flags* flag, va_list* arg);
-void pars_flags_dlina(char chr, flags* flag);
+void pars_flags_length(char chr, flags* flag);
 int pars_width_tochnost(const char* format, int* i);
 int define_specificator(char chr, flags flag, va_list* arg, char* buf);
 
 int specificator_di(flags flag, va_list* arg, char* buf);
 char* number_di_to_string(long number, flags flag);
 int long_to_string(char* mas_for_number, int* index, long number);
-char* rabota_tochnost(flags flag, int zero, int dlina, char* mas_for_number,
+char* rabota_tochnost(flags flag, int zero, int length, char* mas_for_number,
                       int index);
-char* zapolnenie_mas_result(int dlina, long number, flags flag,
+char* zapolnenie_mas_result(int length, long number, flags flag,
                             const char* mas_for_number);
-char* rabota_width(flags flag, char* string, int dlina);
+char* rabota_width(flags flag, char* string, int length);
 
 int specificator_uxXo(flags flag, va_list* arg, char* buf, char chr);
 char* number_uxXo_to_string(unsigned long number, flags flag, int base,
                             char chr);
-int rabota_reshetka(int dlina, int base, char* mas_for_number, char chr,
+int rabota_reshetka(int length, int base, char* mas_for_number, char chr,
                     unsigned long number);
 
 int specificator_c(flags flag, va_list* arg, char* buf);
 int specificator_s(flags flag, va_list* arg, char* buf);
 char* s_null(char* string, int* flag_null);
-char* s_width(char* string, int dlina, flags flag);
+char* s_width(char* string, int length, flags flag);
 
 void specificator_n(va_list* arg, const char* buf);
 int specificator_p(va_list* arg, char* buf, flags flag);
@@ -61,12 +61,12 @@ void NanAndInf(char chr, long double number, char* buf, flags flag,
 char* rabota_mantisa(char* string, char chr, int mantis);
 int expanent(long double* number, int mantis);
 int remove_0(char* string);
-void round_my(char* tmp_mas_for_round, int dlina, int flag_bank);
+void round_my(char* tmp_mas_for_round, int length, int flag_bank);
 void reverse_str(char* str);
 void left_part_to_str(long double number, char* str, flags* flag);
 void right_part_to_str(long double right_part, char* str, int toch);
 void g_and_round(int tmp_tochnost, char* mas_for_left);
 void net_toch_v_float(char* mas_for_left, flags flag);
-void cpy_to_str(const char* tmp_mas_for_round, char* mas_for_left, int dlina,
+void cpy_to_str(const char* tmp_mas_for_round, char* mas_for_left, int length,
                 int* flag_round);
 #endif
